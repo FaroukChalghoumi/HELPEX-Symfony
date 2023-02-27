64507131133939b56f67b53fbb7b6be774916687
Oussema Ayari <80320778+oussema-bit@users.noreply.github.com>
2023-02-27 13:39:11 +0100

    commit integration oussema (missing isgranted permissions)

--- migrations/Version20230227103216.php
+++ migrations/Version20230227103216.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230226215613 extends AbstractMigration
+final class Version20230227103216 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,6 +21,7 @@ final class Version20230226215613 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE accompagnement (id INT AUTO_INCREMENT NOT NULL, task_id INT NOT NULL, user_id INT NOT NULL, user_pro_id INT NOT NULL, is_accepted TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_2130A05B8DB60186 (task_id), INDEX IDX_2130A05BA76ED395 (user_id), INDEX IDX_2130A05BA6E5FF87 (user_pro_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE caisse_organisation (id INT AUTO_INCREMENT NOT NULL, organisation_id INT NOT NULL, montant_caisse_org DOUBLE PRECISION DEFAULT NULL, goal DOUBLE PRECISION NOT NULL, description VARCHAR(255) NOT NULL, INDEX IDX_5227387D9E6B1585 (organisation_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE categorie_formation (id INT AUTO_INCREMENT NOT NULL, nom_categorie_formation VARCHAR(255) NOT NULL, description_categorie_formation VARCHAR(500) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE categorie_produit (id INT AUTO_INCREMENT NOT NULL, nom_cat_produit VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE categorieposte (id INT AUTO_INCREMENT NOT NULL, topic VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -30,6 +31,7 @@ final class Version20230226215613 extends AbstractMigration
         $this->addSql('CREATE TABLE formation (id INT AUTO_INCREMENT NOT NULL, id_categorie_formation_id INT DEFAULT NULL, id_centre_id INT DEFAULT NULL, nom_formation VARCHAR(255) NOT NULL, description_formation VARCHAR(500) NOT NULL, cout_formation DOUBLE PRECISION NOT NULL, nombre_de_place INT DEFAULT NULL, duree VARCHAR(255) DEFAULT NULL, iamgeformation VARCHAR(255) DEFAULT NULL, INDEX IDX_404021BF2BD2D93B (id_categorie_formation_id), INDEX IDX_404021BFC6096BE4 (id_centre_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE inscription_formation (id INT AUTO_INCREMENT NOT NULL, formations_id INT DEFAULT NULL, user_id INT DEFAULT NULL, date_inscription_formation DATE DEFAULT NULL, status_formation VARCHAR(255) DEFAULT NULL, note INT DEFAULT NULL, INDEX IDX_E655E3A73BF5B0C2 (formations_id), INDEX IDX_E655E3A7A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE item (id INT AUTO_INCREMENT NOT NULL, tasks_id INT DEFAULT NULL, titre VARCHAR(255) DEFAULT NULL, time TIME NOT NULL, is_complete TINYINT(1) NOT NULL, photo VARCHAR(255) DEFAULT NULL, INDEX IDX_1F1B251EE3272D31 (tasks_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE organisation (id INT AUTO_INCREMENT NOT NULL, description_organisation VARCHAR(255) NOT NULL, email_organisation VARCHAR(255) NOT NULL, num_tel_organisation VARCHAR(255) NOT NULL, document_organisation VARCHAR(255) NOT NULL, payment_info VARCHAR(255) NOT NULL, nom_org VARCHAR(255) NOT NULL, logo_org VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE poste (id INT AUTO_INCREMENT NOT NULL, categorie_id INT DEFAULT NULL, user_id INT DEFAULT NULL, titre VARCHAR(255) DEFAULT NULL, date DATE NOT NULL, description VARCHAR(255) DEFAULT NULL, multimedia VARCHAR(255) DEFAULT NULL, compteurvote INT NOT NULL, INDEX IDX_7C890FABBCF5E72D (categorie_id), INDEX IDX_7C890FABA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE produits (id INT AUTO_INCREMENT NOT NULL, categorie_produit_id INT DEFAULT NULL, user_id INT DEFAULT NULL, nom_produit VARCHAR(255) NOT NULL, etat_produit VARCHAR(255) NOT NULL, prix_produit VARCHAR(255) NOT NULL, description_produit VARCHAR(255) DEFAULT NULL, image_path VARCHAR(255) DEFAULT NULL, status_produit VARCHAR(255) DEFAULT NULL, localisation_produit VARCHAR(255) DEFAULT NULL, brand VARCHAR(255) DEFAULT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, details VARCHAR(255) DEFAULT NULL, authorisation TINYINT(1) NOT NULL, INDEX IDX_BE2DDF8C91FDB457 (categorie_produit_id), INDEX IDX_BE2DDF8CA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tasks (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) DEFAULT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, is_valid TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -38,6 +40,7 @@ final class Version20230226215613 extends AbstractMigration
         $this->addSql('ALTER TABLE accompagnement ADD CONSTRAINT FK_2130A05B8DB60186 FOREIGN KEY (task_id) REFERENCES tasks (id)');
         $this->addSql('ALTER TABLE accompagnement ADD CONSTRAINT FK_2130A05BA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE accompagnement ADD CONSTRAINT FK_2130A05BA6E5FF87 FOREIGN KEY (user_pro_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE caisse_organisation ADD CONSTRAINT FK_5227387D9E6B1585 FOREIGN KEY (organisation_id) REFERENCES organisation (id)');
         $this->addSql('ALTER TABLE commentaire ADD CONSTRAINT FK_67F068BCA0905086 FOREIGN KEY (poste_id) REFERENCES poste (id)');
         $this->addSql('ALTER TABLE formation ADD CONSTRAINT FK_404021BF2BD2D93B FOREIGN KEY (id_categorie_formation_id) REFERENCES categorie_formation (id)');
         $this->addSql('ALTER TABLE formation ADD CONSTRAINT FK_404021BFC6096BE4 FOREIGN KEY (id_centre_id) REFERENCES centre (id)');
@@ -58,6 +61,7 @@ final class Version20230226215613 extends AbstractMigration
         $this->addSql('ALTER TABLE accompagnement DROP FOREIGN KEY FK_2130A05B8DB60186');
         $this->addSql('ALTER TABLE accompagnement DROP FOREIGN KEY FK_2130A05BA76ED395');
         $this->addSql('ALTER TABLE accompagnement DROP FOREIGN KEY FK_2130A05BA6E5FF87');
+        $this->addSql('ALTER TABLE caisse_organisation DROP FOREIGN KEY FK_5227387D9E6B1585');
         $this->addSql('ALTER TABLE commentaire DROP FOREIGN KEY FK_67F068BCA0905086');
         $this->addSql('ALTER TABLE formation DROP FOREIGN KEY FK_404021BF2BD2D93B');
         $this->addSql('ALTER TABLE formation DROP FOREIGN KEY FK_404021BFC6096BE4');
@@ -71,6 +75,7 @@ final class Version20230226215613 extends AbstractMigration
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649180AA129');
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D6498E768805');
         $this->addSql('DROP TABLE accompagnement');
+        $this->addSql('DROP TABLE caisse_organisation');
         $this->addSql('DROP TABLE categorie_formation');
         $this->addSql('DROP TABLE categorie_produit');
         $this->addSql('DROP TABLE categorieposte');
@@ -80,6 +85,7 @@ final class Version20230226215613 extends AbstractMigration
         $this->addSql('DROP TABLE formation');
         $this->addSql('DROP TABLE inscription_formation');
         $this->addSql('DROP TABLE item');
+        $this->addSql('DROP TABLE organisation');
         $this->addSql('DROP TABLE poste');
         $this->addSql('DROP TABLE produits');
         $this->addSql('DROP TABLE tasks');
