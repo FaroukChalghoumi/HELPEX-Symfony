138eedc2b63f2b40718a4a3fa80e6d9e19c79f84
Oussema Ayari <80320778+oussema-bit@users.noreply.github.com>
2023-03-02 10:03:59 +0100

    commit mail test

--- src/Controller/DonationsController.php
+++ src/Controller/DonationsController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Repository\CaisseOrganisationRepository;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DonationsController extends AbstractController
@@ -12,6 +15,22 @@ class DonationsController extends AbstractController
     #[Route('/donations', name: 'app_donations')]
     public function index(CaisseOrganisationRepository $caisseOrganisationRepository): Response
     {
+        return $this->render('donations/index.html.twig', [
+            'Caisses' => $caisseOrganisationRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/payment', name: 'app_donations_payment')]
+    public function Payment(CaisseOrganisationRepository $caisseOrganisationRepository,MailerInterface $mailer): Response
+    {
+        $email = (new TemplatedEmail());
+
+        $email->subject('Demo message using the Symfony Mailer library.');
+        $email->from('oussema.ayari.2001@gmail.com');
+        $email->to('oussama.ayari1@esprit.tn');
+        $email->text('This is an important message!');
+        $mailer->send($email);
+
         return $this->render('donations/index.html.twig', [
             'Caisses' => $caisseOrganisationRepository->findAll(),
         ]);
--- src/Controller/OrganisationJsonController.php
+++ src/Controller/OrganisationJsonController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Normalizer\NormalizableInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use GuzzleHttp\Client;
 
