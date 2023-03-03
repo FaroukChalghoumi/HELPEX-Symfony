66073bfcf2cab32eca772160f0be25a50087ef0a
FaroukChalghoumi <84229390+FaroukChalghoumi@users.noreply.github.com>
2023-03-03 16:20:22 +0100

    I'm fixing ParaShop's styling ( w li mezzel ymess css rany .... )

--- migrations/Version20230301190654.php
+++ migrations/Version20230301190654.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230228184435 extends AbstractMigration
+final class Version20230301190654 extends AbstractMigration
 {
     public function getDescription(): string
     {
