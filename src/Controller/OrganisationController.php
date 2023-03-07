1c8a4964f457c64f07c76ef430100ba1b3686973
Oussema Ayari <80320778+oussema-bit@users.noreply.github.com>
2023-03-07 15:12:19 +0100

    commit stat

--- src/Controller/OrganisationController.php
+++ src/Controller/OrganisationController.php
@@ -21,8 +21,28 @@ class OrganisationController extends AbstractController
     #[Route('/', name: 'app_organisation_index', methods: ['GET'])]
     public function index(OrganisationRepository $organisationRepository): Response
     {
+        $organisations= $organisationRepository->findAll();
+        $data= [];
+        $orgsNames=array();
+        foreach ($organisations as $organisation){
+            $orgsNames[]=$organisation->getNomOrg();
+            $caisseData=$organisation->getCaisses();
+            $sum=0;
+            foreach ($caisseData as $caisse){
+                $sum+=$caisse->getMontantCaisseOrg();
+            }
+            $data[]=$sum;
+        }
+        $numDataPoints = count($data);
+        $colors = array();
+        for ($i = 0; $i < $numDataPoints; $i++) {
+            $colors[] = "rgba(" . rand(0, 255) . ", " . rand(0, 255) . ", " . rand(0, 255) . ", 0.2)";
+        }
         return $this->render('organisation/index.html.twig', [
-            'organisations' => $organisationRepository->findAll(),
+            'organisations' => $organisations,
+            'orgNames' => json_encode($orgsNames),
+            'statData' => $data,
+            'colors' => json_encode($colors),
         ]);
     }
 
--- src/Controller/OrganisationJsonController.php
+++ src/Controller/OrganisationJsonController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use GuzzleHttp\Client;
 
@@ -34,7 +35,7 @@ class OrganisationJsonController extends AbstractController
     public function new( Request $request,OrganisationRepository $organisationRepository,SerializerInterface $serializer): Response
     {
         $organisation = new Organisation();
-        $organisation->setId($request->get('id'));
+
         $organisation->setEmailOrganisation($request->get('email_organisation'));
         $organisation->setNomOrg($request->get('nom_org'));
         $organisation->setPaymentInfo($request->get('payment_info'));
@@ -43,5 +44,41 @@ class OrganisationJsonController extends AbstractController
         $organisationRepository->save($organisation,true);
         $data=$serializer->normalize($organisation,'json',['groups'=> 'post:read']);
         return new Response(json_encode($data));
+    }
+    //***mobilejson**
+    #[Route("/deleteorgjson/{id}", name: "deletecentrejson")]
+    public function deletecentrejson(Request $req, $id, NormalizerInterface $Normalizer)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $centre = $em->getRepository(Organisation::class)->find($id);
+        $em->remove($centre);
+        $em->flush();
+        $jsonContent = $Normalizer->normalize($centre, 'json', ['groups' => "post:read"]);
+        return new Response("centre deleted successfully " . json_encode($jsonContent));
+    }
+
+
+    #[Route("/updateorgjson/{id}", name: "updatecentrejson")]
+    public function updateCentreJSON(Request $request, $id, NormalizerInterface $Normalizer)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $organisation = $em->getRepository(Organisation::class)->find($id);
+        if($request->get('email_organisation'))
+        $organisation->setEmailOrganisation($request->get('email_organisation'));
+        if($request->get('nom_org'))
+        $organisation->setNomOrg($request->get('nom_org'));
+        if($request->get('payment_info'))
+        $organisation->setPaymentInfo($request->get('payment_info'));
+        if($request->get('num_tel_organisation'))
+        $organisation->setNumTelOrganisation($request->get('num_tel_organisation'));
+        if($request->get('description_organisation'))
+        $organisation->setDescriptionOrganisation($request->get('description_organisation'));
+
+        $em->flush();
+
+        $jsonContent = $Normalizer->normalize($organisation, 'json', ['groups' => "post:read"]);
+        return new Response("centre updated successfully " . json_encode($jsonContent));
     }
 }
